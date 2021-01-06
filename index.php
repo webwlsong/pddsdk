b44735e28435d4fdd696c0ac4aa86ce8694ae5fc
WeiLi <webwlsong@vip.qq.com>
2021-01-06 16:13:15 +0800

    first

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require "vendor/autoload.php";
+
+$test = new pddsdk\Test();
+$test->test();
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'webwlsong/php_pddsdk',
+  ),
+  'versions' => 
+  array (
+    'webwlsong/php_pddsdk' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
